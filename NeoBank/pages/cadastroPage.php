e70d77229a5760f80edda206755b373597b11ee6
GustavoTorelli <gustavo.torelli17@gmail.com>
2024-11-01 17:15:28 -0300

    jhhjh
    cadastro enviado

---
+++ NeoBank/pages/cadastroPage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <title>Login</title>
+</head>
+<body>
+     
+<header><!--NavBar, aqui está localizado o cabeçalho do site -->
+        <div>
+            <h1>Logo</h1> <!--Futuro-Logo-->
+            <nav>
+                <ul>
+                    <li><a href="#">Página Inicial</a></li>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Conta Digital</a></li>
+                    <li><a href="#">Ajuda</a></li>
+                </ul>
+            </nav>
+            </div>
+    
+        </header>
+    <main>
+        <div class="card"><!--Login, aqui está localizado o card de login do site -->
+            <!--colocar logo-->
+            <h1>Cadastro</h1>
+            <form action="#" method="post">
+                <input placeholder="Nome Completo" type="text" name="NomCompleto">
+                <input placeholder="CPf" type="number" name="Cpf">
+                <input placeholder="Email" type="email" name="Email">
+                <input placeholder="Senha"type="password" name="Senha">
+                <button type="submit" value="Entrar">Enviar</button>
+            </form>
+           
+        
+        </div>
+    </main>
+    
+</body>
+</html>
